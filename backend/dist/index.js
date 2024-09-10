6b07835fe0f26fabdeb21a9d60b1f3f84e7abf87
Ag18231766 <amartyagupta18231766@gmail.com>
2024-09-10 10:35:27 +0530

    adding cors

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 3000;
 app.use('/api/v1', routes_1.default);
+app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import RootRouter from "./routes";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app: Express = express();
 const port = 3000;
 
 app.use('/api/v1',RootRouter);
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
